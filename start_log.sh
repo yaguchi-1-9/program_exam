7efea222a7b3a191a1a704427110162bb22fe020
yaguchi-1-9 <55125138+yaguchi-1-9@users.noreply.github.com>
2021-12-06 17:01:50 +0900

    Update start_log.sh

--- start_log.sh
+++ start_log.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-#cat /dev/null > log/test.log
-python3 log_maker.py >> log/test.log
+# LOGを新規作成
+# python3 log_maker.py > log/test.log
 
+# LOGを随時追加
+python3 log_maker.py >> log/test.log
